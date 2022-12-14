0ca20dc2375775b60f73100428121e7f4848a5c5
IreneB99 <irene.bortolotti99@gmail.com>
2022-12-14 12:38:09 +0100

    aggiornamento file javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+document.querySelector("#green").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#red").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#black").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#blue").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+document.querySelector("#yellow").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
+
+
+document.querySelector("#pink").addEventListener("click", function(e){
+    let color = e.target.id
+    body.style.backgroundColor = color
+})
